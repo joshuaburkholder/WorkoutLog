addac491e13f3d58f880dcb615e5b78d73e2ec2f
Joshua Burkholder <Joshua Burkholder>
2017-01-30 13:52:04 -0500

    added reflections

--- client/app.js
+++ client/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$("#testAPI").on("click", function(){
 		console.log("It is working!");
-	});
+	});										//this function creates a way to "test" to make sure it is working correctly
 	var test=$.ajax({
 		type: "GET",
 		url: "http://localhost:3000/api/test"
-	});
-	test.done(function(data){
+	});										//this function obtains the information form localhost:3000
+	test.done(function(data){				//the following runs indication on success or failure of prev. command
 		console.log(data);
-	});
+	});										
 	test.fail(function(){
 		console.log("Oh no!");
 	})
