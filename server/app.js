1a5642050a38f5ce44d7a086bd764ea2f9d89806
Joshua Burkholder <Joshua Burkholder>
2017-01-30 16:15:43 -0500

    adding models folder

--- server/app.js
+++ server/app.js
@@ -1,27 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var Sequelize = require('sequelize');
+var sequelize = require('./db');
 
-var sequelize = new Sequelize('workoutlog', 'postgres', 'H438#sk!fDf033R', {
-	host: 'localhost',
-	dialect: 'postgres'
-});
-
-sequelize.authenticate().then(
-	function(){
-		console.log('connected to workoutlog postgres db');
-	},
-	function(err){
-		console.log(err);
-	}
-);
-
-
-var User = sequelize.define('user',{
-	username: Sequelize.STRING,
-	passwordhash: Sequelize.STRING,
-});
+var User = sequelize.import('./models/user');
 
 
 
