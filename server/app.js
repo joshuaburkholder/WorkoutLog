292c75d194852a340cb75e67d5a6e0f1552d82db
Joshua Burkholder <Joshua Burkholder>
2017-01-30 14:24:17 -0500

    update

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000,function(){						//function begins to run server on local:3000
-	console.log("app is listening on 3000");	//console log indicates whether it was successful
+app.listen(3000,function(){						
+	console.log("app is listening on 3000");	
 });
 
 app.use('/api/test', function(req, res){
