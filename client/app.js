d4c622c529cd30f47f6fde022d39869399d716bd
Joshua Burkholder <Joshua Burkholder>
2017-01-30 14:24:38 -0500

    update

--- client/app.js
+++ client/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	$("#testAPI").on("click", function(){
 		console.log("It is working!");
-	});										//this function creates a way to "test" to make sure it is working correctly
+	});										
 	var test=$.ajax({
 		type: "GET",
 		url: "http://localhost:3000/api/test"
-	});										//this function obtains the information form localhost:3000
-	test.done(function(data){				//the following runs indication on success or failure of prev. command
+	});										
+	test.done(function(data){				
 		console.log(data);
 	});										
 	test.fail(function(){
