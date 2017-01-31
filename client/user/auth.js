48529394204bb21faada308333033a89a8b4b28b
Joshua Burkholder <Joshua Burkholder>
2017-01-31 11:33:01 -0500

    added user and auth files

---
+++ client/user/auth.js
@@ -0,0 +1,50 @@
+$(function(){
+	$.extend(WorkoutLog, {
+		//sign up method
+		signup: function(){
+			//username & password variables.
+			var username = $("#su_username").val();
+			var password = $("#su_password").val();
+			//user object
+			var user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+
+			//sign up post
+			var signup = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+
+			//signup done/fail
+			signup.done(function(data){
+				if(data.sessionToken){
+					WorkoutLog.setAuthHeader(data.sessionToken);
+				}
+
+				$("#signup-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$("#loginout").text("Logout");
+				}).fail(function(){
+				$("#su_error").text("There was an issue with your sign up").show();
+				});
+
+			})
+		}
+
+		//login method
+
+		//loginout method
+
+
+	});
+
+	//bind events
+	$("#signup").on("click", WorkoutLog.signup);
+
+});
