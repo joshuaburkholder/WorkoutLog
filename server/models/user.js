fddf6dc76298ad3f48682da8c55dfd519f9e4b53
Joshua Burkholder <Joshua Burkholder>
2017-02-02 13:44:22 -0500

    added comments

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,5 @@
-//user model created using sequelize
-//talks to the table user
 
+//var Sequelize = require('sequelize');
 
 module.exports = function(sequelize, DataTypes){
 	return sequelize.define('user', {
